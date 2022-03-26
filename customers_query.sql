3df701739714cfa7eddcf217f4e5ee5c6ed3a788
Kuasar1 <75116758+Kuasar1@users.noreply.github.com>
2022-03-26 15:51:20 +0800

    Added customers insert query

---
+++ customers_query.sql
@@ -0,0 +1,9 @@
+/*
+-- Query: select * from customers
+LIMIT 0, 1000
+
+-- Date: 2022-03-26 15:50
+*/
+INSERT INTO `` (`id`,`address_status`,`address_type`,`birth_date`,`building`,`city`,`country`,`district`,`door_number`,`email`,`email_status`,`email_type`,`first_name`,`floor`,`gender`,`last_name`,`password`,`phone_number`,`phone_prefix`,`phone_status`,`phone_type`,`state`,`status`,`street`,`user_name`) VALUES (1,NULL,NULL,'2000-09-13',NULL,NULL,NULL,NULL,NULL,'anudar666@gmail.com',NULL,NULL,'Chinzorig',NULL,'M','Javzandolgor','$2a$10$y38zczb46d0UbhDz1CO5HOTRJs4Us1D0JVLQmk8uB7pRlOAIwUa2.','88550558',NULL,NULL,NULL,'T','ACTIVE',NULL,'admin');
+INSERT INTO `` (`id`,`address_status`,`address_type`,`birth_date`,`building`,`city`,`country`,`district`,`door_number`,`email`,`email_status`,`email_type`,`first_name`,`floor`,`gender`,`last_name`,`password`,`phone_number`,`phone_prefix`,`phone_status`,`phone_type`,`state`,`status`,`street`,`user_name`) VALUES (2,NULL,NULL,'2001-05-22',NULL,NULL,NULL,NULL,NULL,'turtogtokh.b@gmail.com',NULL,NULL,'Turtogtokh',NULL,'M','Burenbat','$2a$10$DdVss1a3ysbtgupxze1gZONvkCQCieQ5AEbGUK8QQ1k5khVhIpJC.','99223344',NULL,NULL,NULL,'T','ACTIVE',NULL,'turuu');
+INSERT INTO `` (`id`,`address_status`,`address_type`,`birth_date`,`building`,`city`,`country`,`district`,`door_number`,`email`,`email_status`,`email_type`,`first_name`,`floor`,`gender`,`last_name`,`password`,`phone_number`,`phone_prefix`,`phone_status`,`phone_type`,`state`,`status`,`street`,`user_name`) VALUES (3,NULL,NULL,'2001-05-26',NULL,NULL,NULL,NULL,NULL,'gianupp@gmail.com',NULL,NULL,'Enguun',NULL,'F','Bilguutei','$2a$10$TsLJBpiziasR8OXSaczpVe8Yp7aDO/AUCUEwAoCZ/799s2ID9fr7G','99837401',NULL,NULL,NULL,'T','ACTIVE',NULL,'enguun');
